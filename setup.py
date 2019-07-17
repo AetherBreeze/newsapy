1296badd124d17a0194679c81f979ad2b43263e6
CocoPommel <theteleforce@gmail.com>
2019-07-17 14:14:10 -0400

    v0.1.8 setup

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ python_requires = '>=3'
 setup(
     name='newsapy',
     packages=["newsapy"],
-    version='0.1.7',
+    version='0.1.8',
     license='MIT',
     url='https://everyonegetinhere.com',
     install_requires=install_requires,
     description='An unofficial asynchronous, key-switching Python client for NewsAPI',
-    download_url='https://github.com/CocoPommel/newsapy/archive/0.1.7.tar.gz',
+    download_url='https://github.com/CocoPommel/newsapy/archive/0.1.8.tar.gz',
     keywords=['newsapy', 'newsapi', 'news'],
     classifiers=[
         'Intended Audience :: Developers',
